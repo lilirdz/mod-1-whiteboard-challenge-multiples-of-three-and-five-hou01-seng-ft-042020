21d317009f4e82ec377be2a65aaa9ffd9b6ab85f
lilirdz <lilimrod@gmail.com>
2020-05-06 13:45:21 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1...limit).to_a.select {|i| i % 3 == 0 || i % 5 == 0}
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
+
